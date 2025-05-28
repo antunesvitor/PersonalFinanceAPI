f6706963280c968e6797a2ceefbecc49c90bf8f1
Vitor Guilherme Antunes <vguilhermeantunes@gmail.com>
2025-05-27 22:04:33 -0300

    first commit: project initialized

---
+++ Controllers/CategoriaController.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PersonalFinanceAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriaController : ControllerBase
+    {
+    }
+}
---
+++ Models/Categoria.cs
@@ -0,0 +1,9 @@
+using System;
+
+namespace PersonalFinanceAPI.Models;
+
+public class Categoria
+{
+    public int Id { get; set;}
+    public string Nome { get; set; }
+}
